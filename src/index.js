b602636306be93264c8badb30dffd6fd9646580f
zzh0c <zzh0czz@gmail.com>
2021-02-04 17:10:05 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LoginScreen from "./components/loginWindow";
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+        <LoginScreen/>
   </React.StrictMode>,
   document.getElementById('root')
 );
