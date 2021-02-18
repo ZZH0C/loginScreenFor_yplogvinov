bc09776e66c09d0c645ff8a5fd4685c97a7fed88
zzh0c <zzh0czz@gmail.com>
2021-02-18 16:48:01 +0300

    feat

--- src/components/loginWindow/index.js
+++ src/components/loginWindow/index.js
@@ -1,34 +1,48 @@
 import React, {useState} from "react";
-import { Form} from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
 
 
 export default function LoginScreen() {
     const [name, setName] = useState("")
     const [password, setPassword] = useState("")
-
     return (
-
-        <Form className="form1">
+        <div className="form1">
             <div className="former">
                 <Form.Group controlId="formBasicName">
                     <div className="centerAlign">
                         <Form.Label><p>Name</p></Form.Label>
-                        <input type='text' placeholder="Enter name" name='qwe' value={name} onChange={e => setName(e.target.value)} />
+                        <input type='text' placeholder="Enter name" name='qwe' value={name}
+                               onChange={e => setName(e.target.value)}/>
                     </div>
                 </Form.Group>
-
                 <Form.Group controlId="formBasicPassword">
                     <div className="centerAlign">
                         <Form.Label><p>Password</p></Form.Label>
-                        <input type='text' placeholder="Password" name='qwe' value={password} onChange={e => setPassword(e.target.value)} />
+                        <input type='password' placeholder="Password" name='qwe' value={password}
+                               onChange={e => setPassword(e.target.value)}/>
                     </div>
                 </Form.Group>
-                <a href={`http://${name}:${password}@localhost/pages`} className='aButton'>Click me!</a>
-
-
-
+                <button
+                    className='custom-button'
+                    onClick={() => login(name, password)}
+                >
+                    Click me!
+                </button>
             </div>
-        </Form>
+        </div>
 
     );
+}
+
+
+function login(name, pass) {
+    console.log(name, pass)
+    let ourLink = `http://${name}:${pass}@localhost/pages`
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', ourLink, false);
+    if (xhr.status >= 200 && xhr.status < 300) {
+        window.location = ourLink;
+    } else {
+        alert("wrong!")
+    }
 }
