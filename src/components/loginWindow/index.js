f4ab092947553bd687b2c17801419276ab3a1ad2
zzh0c <zzh0czz@gmail.com>
2021-03-01 14:27:02 +0300

    feat: add alert message

--- src/components/loginWindow/index.js
+++ src/components/loginWindow/index.js
@@ -5,28 +5,62 @@ import {Form} from 'react-bootstrap';
 export default function LoginScreen() {
     const [name, setName] = useState("")
     const [password, setPassword] = useState("")
+    const [styles,setStyles] = useState({
+        inputs: 'input-default',
+        alertText: 'alert-text hidden'
+    })
+
+
+    function alertUser() {
+        setStyles({
+            inputs: 'input-alert',
+            alertText: 'alert-text'
+        })
+    }
+
+
+    function login(name, pass) {
+        console.log(name, pass)
+        let ourLink = `http://${name}:${pass}@localhost/pages`
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', ourLink, false);
+        if (xhr.status >= 200 && xhr.status < 300) {
+            window.location = ourLink;
+        } else {
+
+            alertUser();
+
+        }
+    }
+
+
     return (
         <div className="form1">
             <div className="former">
-                <Form.Group controlId="formBasicName">
-                    <div className="centerAlign">
-                        <Form.Label><p>Name</p></Form.Label>
-                        <input type='text' placeholder="Enter name" name='qwe' value={name}
-                               onChange={e => setName(e.target.value)}/>
-                    </div>
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <div className="centerAlign">
-                        <Form.Label><p>Password</p></Form.Label>
-                        <input type='password' placeholder="Password" name='qwe' value={password}
-                               onChange={e => setPassword(e.target.value)}/>
-                    </div>
-                </Form.Group>
+                <div className={'form-text-area-container'}>
+                    <Form.Group controlId="formBasicName">
+                        <div className="centerAlign">
+                            <Form.Label><p>Name</p></Form.Label>
+                            <input className={styles.inputs} type='text' placeholder="Enter name" name='qwe' value={name}
+                                   onChange={e => setName(e.target.value)}/>
+                        </div>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <div className="centerAlign">
+                            <Form.Label><p>Password</p></Form.Label>
+                            <input className={styles.inputs} type='password' placeholder="Password" name='qwe' value={password}
+                                   onChange={e => setPassword(e.target.value)}/>
+                        </div>
+                    </Form.Group>
+                </div>
+                <div className={styles.alertText}>
+                    Please enter valid name and password!
+                </div>
                 <button
                     className='custom-button'
                     onClick={() => login(name, password)}
                 >
-                    Click me!
+                    Sign In
                 </button>
             </div>
         </div>
@@ -35,14 +69,4 @@ export default function LoginScreen() {
 }
 
 
-function login(name, pass) {
-    console.log(name, pass)
-    let ourLink = `http://${name}:${pass}@localhost/pages`
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', ourLink, false);
-    if (xhr.status >= 200 && xhr.status < 300) {
-        window.location = ourLink;
-    } else {
-        alert("wrong!")
-    }
-}
+
